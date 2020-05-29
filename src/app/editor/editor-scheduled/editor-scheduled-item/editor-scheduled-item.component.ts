830209f1ae5f6c2396839f699fe600ff6f7eead7
Cara Lagumen <CaraLagumen@gmail.com>
2020-05-29 16:17:51 -0700

    styled scheduled editor

--- src/app/editor/editor-scheduled/editor-scheduled-item/editor-scheduled-item.component.ts
+++ src/app/editor/editor-scheduled/editor-scheduled-item/editor-scheduled-item.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
+import { CalendarService } from "../../../calendar/calendar.service";
 import { Employee } from "../../../shared/models/users/employee.model";
 import { Shift } from "../../../shared/models/shift/shift.model";
 import { WeeklyShift } from "../../../shared/models/shift/weekly-shift.model";
 import { WeeklyScheduled } from "../../../shared/models/shift/weekly-scheduled.model";
+import { ShiftProperties } from "../../../shared/tools/custom-classes";
 
 @Component({
   selector: "app-editor-scheduled-item",
@@ -11,19 +13,29 @@ import { WeeklyScheduled } from "../../../shared/models/shift/weekly-scheduled.m
   styleUrls: ["./editor-scheduled-item.component.scss"],
 })
 export class EditorScheduledItemComponent implements OnInit {
-  @Input() employees: Employee[];
-  @Input() shifts: Shift[];
-  @Input() weeklyShifts: WeeklyShift[];
-  @Input() weeklyScheduled: WeeklyScheduled[];
+  @Input() employees$: Employee[];
+  @Input() shifts$: Shift[];
+  @Input() weeklyShifts$: WeeklyShift[];
+  @Input() weeklyScheduled$: WeeklyScheduled[];
   @Input() days: number[];
 
   @Output() editorScheduledEmitter = new EventEmitter<
     [Shift | WeeklyShift | WeeklyScheduled | Employee, string]
   >();
 
-  constructor() {}
+  daysInWords = ShiftProperties.daysInWords;
+
+  constructor(private calendarService: CalendarService) {}
 
   ngOnInit() {}
+
+  getFormattedHour(hour: number) {
+    return this.calendarService.getFormattedHour(hour);
+  }
+
+  getFormattedLocation(location: string) {
+    return this.calendarService.getFormattedLocation(location);
+  }
 
   onSelectShift(shift: Shift) {
     this.editorScheduledEmitter.emit([shift, `onSelectShift`]);
